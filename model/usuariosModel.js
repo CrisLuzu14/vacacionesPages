ed605a29c556a41d72816b01eeb73d8538e88a19
StalinSoriano <david.2013681@gmail.com>
2021-08-28 17:56:32 -0500

    creacion de rutas

--- model/usuariosModel.js
+++ model/usuariosModel.js
@@ -1,9 +1,23 @@
 const mongoose= require('mongoose')
 
 const UsuariosSchema = new mongoose.Schema({
-   folio: String,
-   norden: String,
-   sucursal: String,
-   cpago: String,
-   fpedido: String,
+   nombres: String,
+   apellidos: String,
+   cedula: {
+      type:String,
+      require:true
+   },
+   correo: String,
+   proceso: String,
+   subproceso:String,
+   puesto:String,
+   lugarTrabajo:String,
+   remuneracionM:Double,
+   estado:{
+      type:String,
+      default:'1',
+   },
+   idUsuario:Object,
+   idSolicitud:Object
+
 })
---
+++ routes/solicitudVacaciones.js
@@ -0,0 +1,24 @@
+//const path= require('path')
+const express = require('express')
+//const rootDir = require('../util/path')
+
+const router=express.Router()
+let user=null;
+router.post('/empleados',(req,res,next)=>{//el métod use recibe 3 paramatros req,res,next
+    //para enviar información al servidor usamos la función send()
+    //res.sendFile(path.join(__dirname,'../','views','add-product.html')) 
+    console.log(req.body.user)
+    user=req.body.user
+    let pass=req.body.pass
+        if(user==='stalin') {
+            if(pass==='123') {
+                res.render('empleados',{'usuario':user})
+                //res.sendFile(path.join(rootDir,'views','add-vacaciones.html')) 
+            }
+        }else{
+            res.redirect('/')
+        }
+   
+} )
+ 
+module.exports=router
