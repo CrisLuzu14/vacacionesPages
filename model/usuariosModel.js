ef833d3c065568212cd897727f2a13b5feab8230
StalinSoriano <david.2013681@gmail.com>
2021-08-29 22:51:33 -0500

    creacion modelo usuarios y consulta find problema al retornar info

--- model/usuariosModel.js
+++ model/usuariosModel.js
@@ -3,21 +3,17 @@ const mongoose= require('mongoose')
 const UsuariosSchema = new mongoose.Schema({
    nombres: String,
    apellidos: String,
-   cedula: {
-      type:String,
-      require:true
-   },
+   cedula: String,
    correo: String,
    proceso: String,
    subproceso:String,
    puesto:String,
    lugarTrabajo:String,
-   remuneracionM:Double,
-   estado:{
-      type:String,
-      default:'1',
-   },
+   remuneracionM:String,
+   estado:String,
    idUsuario:Object,
-   idSolicitud:Object
+   idSolicitud:Object,
 
 })
+
+module.exports=mongoose.model('usuarios',UsuariosSchema)
--- routes/empleados.js
+++ routes/empleados.js
@@ -1,10 +1,10 @@
 //const path= require('path')
 const express = require('express')
 //const rootDir = require('../util/path')
-
+const Usuarios=require('../model/usuariosModel')
 const router=express.Router()
 let user=null;
-router.post('/empleados',(req,res,next)=>{//el métod use recibe 3 paramatros req,res,next
+router.post('/empleados',async (req,res,next)=>{//el métod use recibe 3 paramatros req,res,next
     //para enviar información al servidor usamos la función send()
     //res.sendFile(path.join(__dirname,'../','views','add-product.html')) 
     console.log(req.body.user)
@@ -12,7 +12,9 @@ router.post('/empleados',(req,res,next)=>{//el métod use recibe 3 paramatros re
     let pass=req.body.pass
         if(user==='stalin') {
             if(pass==='123') {
-                res.render('empleados',{'usuario':user})
+               const usuarios =await Usuarios.find()
+                console.log(usuarios)
+                res.render('menu',{'usuario':user})
                 //res.sendFile(path.join(rootDir,'views','add-vacaciones.html')) 
             }else{
                 res.redirect('/')
@@ -21,6 +23,9 @@ router.post('/empleados',(req,res,next)=>{//el métod use recibe 3 paramatros re
             res.redirect('/')
         }
    
+} )
+router.get('/empleados2',(req,res,next)=>{
+                res.render('menu',{'usuario':user})   
 } )
  
 module.exports=router
