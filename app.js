3090bb36f0bf88b5227f62a15c6ece2ec77b62dc
CrisLuzu14 <89538297+CrisLuzu14@users.noreply.github.com>
2021-09-06 14:42:53 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
-=======
 
 //import libreries
->>>>>>> dev/soriano
 const path = require('path');
 const express= require('express');//importa la libreria express
 const amdinRoute= require('./routes/admin')
@@ -14,25 +9,12 @@ const mongoose = require('mongoose')
 
 
 const app =express();//llamas al metodo express
-<<<<<<< HEAD
-const homeRoute= require('./hola.js/home')
-const vacacionesRoute=require('./hola.js/vacaciones');
-const { use } = require('./hola.js/home');
-app.use(express.urlencoded({extended:true})) //sirve para poder tomar los argumentos del get o post
-app.use(homeRoute)
-app.use(vacacionesRoute)
-app.use(morgan)('dev');
-app.use(express.urlencoded({extend: false}))
-app.use((req,res,next)=>{
-    
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-=======
 
 //connection DataBase MongoDB
-const mongooseUrl='mongodb+srv://TalentHuman:TalentHuman@vacaciones.qspdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongooseUrl='mongodb+srv://TalentHuman:TalentHuman@vacaciones.qspdv.mongodb.net/vacacionesTalentHuman?retryWrites=true&w=majority'
 mongoose.connect(mongooseUrl,{
     useNewUrlParser:true,
-    useUnifiedTopology:true,
+    useUnifiedTopology:true, 
 })
 mongoose.connection.on('connected',()=>{//MUESTRA MENSAJE SI CONNECCTION FUE OK
     console.log('mongoose connect')
@@ -46,15 +28,14 @@ app.use(express.urlencoded({extended:true})) //sirve para poder tomar los argume
 app.use(amdinRoute)
 app.use(empleadosRoute)
 app.use(solicitudRoute)
-
+app.use(express.static(__dirname+'/public'))//para poder usar los recursos como img js css
 //PAGE NOT FOUND
 app.use((req,res,next)=>{
     res.status(400).render('404')
     //res.status(404).sendFile(path.join(__dirname,'views','404.html'))
->>>>>>> dev/soriano
-})
+})  
 app.listen(3000)//de esta manera queda mas simple y ya no se necesita utilizar el require('http')
-// npm init -f
+// npm init -f 
 // npm install --save express
 // npm install --save-dev nodemon
 // agregar script package.json "start":"nodemon app.js"
